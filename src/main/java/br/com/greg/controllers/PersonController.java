5e6f53b068a58ff582e6705fb32f64738a4536a4
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-19 09:59:43 -0300

    first commit

---
+++ src/main/java/br/com/greg/controllers/PersonController.java
@@ -0,0 +1,39 @@
+package br.com.greg.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.greg.model.Person;
+import br.com.greg.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices services;
+	
+	@RequestMapping(
+			value="/{id}",
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE
+		)
+	public Person findById(@PathVariable("id") String id) {
+		return services.findById(id);
+	}
+	
+	@RequestMapping(
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE
+			)
+	public List<Person> findAll() {
+		return services.findAll();
+	}
+}
---
+++ src/main/java/br/com/greg/services/PersonServices.java
@@ -0,0 +1,53 @@
+package br.com.greg.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.greg.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("João");
+		person.setLastName("Silva");
+		person.setAddress("João Pessoa - PB - Brasil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name"+i);
+		person.setLastName("Last name"+i);
+		person.setAddress("Soma address in Brasil" +i);
+		
+		if(i % 2 == 0) {
+			person.setGender("Female");			
+		} else {
+			person.setGender("Male");		
+		}
+		
+		return person;
+	}
+}
