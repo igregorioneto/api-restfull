fbd713c7b20d375f0e0e06b34ab458e2555cfc21
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-21 10:32:29 -0300

    APIRESTFULL - Migrations e JSON Serialization

--- src/main/java/br/com/greg/controllers/PersonController.java
+++ src/main/java/br/com/greg/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.greg.data.vo.v2.PersonVOV2;
 import br.com.greg.services.PersonServices;
 
 @RestController
-@RequestMapping("/PersonVO")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired
--- src/main/java/br/com/greg/data/vo/PersonVO.java
+++ src/main/java/br/com/greg/data/vo/PersonVO.java
@@ -10,14 +10,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
+	private Long id;	
+	@JsonProperty("first_name")
 	private String firstName;
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+	@JsonIgnore
 	private String gender;
 	
 	public PersonVO() {
