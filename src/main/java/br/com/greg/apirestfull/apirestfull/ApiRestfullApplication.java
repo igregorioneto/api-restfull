db48cbbd4037b05e944d8350e6dd9a4eb7bc7809
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-24 17:45:39 -0300

    APIRESTFULL - Encripitando e Decodificando JWT

--- src/main/java/br/com/greg/apirestfull/apirestfull/ApiRestfullApplication.java
+++ src/main/java/br/com/greg/apirestfull/apirestfull/ApiRestfullApplication.java
@@ -1,9 +1,13 @@
 package br.com.greg.apirestfull.apirestfull;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class ApiRestfullApplication {
 
 	public static void main(String[] args) {
