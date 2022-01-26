b7ebb4bbb6045016769791229f39e21626056e2e
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-26 18:05:01 -0300

    APIRESTFULL - Configurações e alterações

--- src/main/java/br/com/greg/converter/custom/BookConverter.java
+++
@@ -1,9 +0,0 @@
-package br.com.greg.converter.custom;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BookConverter {
-
-	
-}
--- src/main/resources/db/migration/mysql/V2_Person_Insert_Table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'Uberlândia - Minas Gerais - Brasil', 'João', 'Male', 'Silva'),
-	(2, 'São Paulo - São Paulo - Brasil', 'Leticia', 'Female', 'Oliveira'),
-	(3, 'João Pessoa - Paraíba - Brasil', 'Fernanda', 'Female', 'Silva');
